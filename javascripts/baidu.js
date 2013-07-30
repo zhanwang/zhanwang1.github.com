b164820a9af6694e60cbcf09776254cb2136bb25
zhanwang <jian_wang_zhan@163.com>
2013-07-30 10:37:49 +0800

    Create baidu.js

---
+++ javascripts/baidu.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F1a6d8a99c09979207d1da05e553372ec' type='text/javascript'%3E%3C/script%3E"));
+</script>
